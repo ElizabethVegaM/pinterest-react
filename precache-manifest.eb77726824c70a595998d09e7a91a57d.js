bf6d33643209a36c7f022e0b1756affa52a4fce3
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-10-07 19:04:47 -0300

    Updates

---
+++ precache-manifest.eb77726824c70a595998d09e7a91a57d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pinterest-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7bd4740084245b895d82",
+    "url": "/pinterest-react/static/js/runtime~main.7bd47400.js"
+  },
+  {
+    "revision": "6209e2beddee48dca6ec",
+    "url": "/pinterest-react/static/js/main.6209e2be.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/pinterest-react/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "6209e2beddee48dca6ec",
+    "url": "/pinterest-react/static/css/main.655be98e.chunk.css"
+  },
+  {
+    "revision": "278c1822496306fec069dcea0e4df599",
+    "url": "/pinterest-react/index.html"
+  }
+];
